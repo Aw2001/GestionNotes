cd4832238bea2ec7b616014d9e9a746a03caa42c
Aw2001 <awafall82817@gmail.com>
2025-03-01 21:22:06 +0000

    tp .NET

---
+++ Etudiant/Etudiant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+    public class Etudiant
+    {
+        public string Nom {  get; set; }
+        public string Prenom { get; set; }
+        public int NO {  get; set; }
+        public double NoteCC { get; set; }
+        public double NoteDevoir {  get; set; }
+
+        // Calcul de la moyenne avec pondération
+        public double Moyenne => (NoteCC * 0.33) + (NoteDevoir * 0.67);
+
+        public override string ToString()
+        {
+            return $"{NO} - {Prenom} {Nom} | CC: {NoteCC} | Devoir: {NoteDevoir} | Moy: {Moyenne:F2}";
+        }
+
+    }
+
+
